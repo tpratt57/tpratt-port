b480e230bc09a50eb8e14c237923ffaa1cc24cb0
unknown <pratttrevor79@gmail.com>
2022-10-09 01:52:52 -0500

    adding services-component styling

--- client/src/Components/Services/Services.jsx
+++ client/src/Components/Services/Services.jsx
@@ -1,8 +1,104 @@
-import React from 'react'
+import React from 'react';
+import { BsCheck } from 'react-icons/bs';
+import './services.css'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='.container services__container'>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATION */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Filler</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WHATEVER THIS IS GONNA BE */}
+      </div>
+
+    </section>
   )
 }
 
